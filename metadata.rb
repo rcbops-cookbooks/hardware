ecc8a9c102bedeaae0f142079b706588db1da0f5
rcbjenkins <rcb-deploy@lists.rackspace.com>
2013-03-20 16:41:25 -0500

    End of sprint version bump.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "rcb-deploy@lists.rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures hardware"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.15"
+version          "0.0.16"
 
 %w{ centos ubuntu }.each do |os|
   supports os
