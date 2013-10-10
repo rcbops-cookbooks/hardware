a27948ecde1e3cedaaa1c9b205ccaabb8409fde4
rackerjoe <breu@breu.org>
2013-10-10 14:00:42 -0500

    Version Bump to 4.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "rcb-deploy@lists.rackspace.com"
 license          "Apache 2.0"
 description      "Installs/Configures hardware"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.0.16"
+version           "4.1.3"
 
 %w{ amazon centos debian fedora oracle redhat scientific ubuntu }.each do |os|
   supports os
